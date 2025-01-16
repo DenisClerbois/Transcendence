cfd65135848cb1b581517383c216733ad58c516e
Denis Clerbois <declerbo@shi-r3-p4.s19.be>
2025-01-16 15:47:14 +0100

    Register view added, missing duplicate management

--- django/app_srcs/urls.py
+++ django/app_srcs/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path("api/login/", views.LoginView, name="login"),
 	path("api/logout/", views.LogoutView, name="logout"),
+	path("api/register/", views.RegisterView, name="register"),
 	path("api/checkUserAuthenticated/", views.checkUserAuthenticated, name="checkUserAuthenticated"),
 ]
--- django/app_srcs/views.py
+++ django/app_srcs/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 import json
+import re
 
 # Create your views here.
 @csrf_exempt
@@ -27,7 +29,30 @@ def LogoutView(request):
 
 
 def checkUserAuthenticated(request):
-    if request.user.is_authenticated:
-        return JsonResponse({'authenticated': True}, status=200)
-    else:
-        return JsonResponse({'authenticated': False}, status=401) # change this to 200 and adapt the js response
+	if request.user.is_authenticated:
+		return JsonResponse({'authenticated': True}, status=200)
+	else:
+		return JsonResponse({'authenticated': False}, status=401) # change this to 200 and adapt the js response
+
+@csrf_exempt
+def RegisterView(request):
+	# Use raw string for regex patterns in Python
+	regex_email = r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+	regex_password = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
+
+	if request.method == 'POST':
+		data = json.loads(request.body)
+		email = data.get('email')
+		username = data.get('username')
+		password = data.get('password')
+
+		# Server-side validation (email, username, and password must be correct)
+		if not re.match(regex_email, email) or not username or not re.match(regex_password, password):
+			return JsonResponse({'message': 'Error on register.'}, status=401)
+
+		# Create the user
+		user = User.objects.create_user(username=username, email=email, password=password)
+		if user is None:
+			return JsonResponse({'message': 'Error on logged in.'}, status=401)
+		login(request, user)
+		return JsonResponse({'message': 'User account created.'}, status=200)
