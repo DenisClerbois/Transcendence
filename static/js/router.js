4425a8b628156d22051c5fea5ba5a501f26d76bf
Denis <declerbo@student.s19.be>
2025-01-22 10:53:10 +0100

    function to find all script in html and add them, js script move into the according html for more clarity

--- static/js/router.js
+++ static/js/router.js
@@ -16,16 +16,13 @@ function route(event) {
 	fetchBody();
 }
 
-
-
+/* Load and execute any script find in the html template */
 function runScriptsInHTML(html) {
-	console.log('test script');
 	const tempDiv = document.createElement('div');
 	tempDiv.innerHTML = html;
 
 	const scripts = tempDiv.querySelectorAll('script');
 	scripts.forEach(script => {
-		console.log(script);
 		const newScript = document.createElement('script');
 		if (script.src)
 			newScript.src = script.src; // External script
@@ -37,50 +34,23 @@ function runScriptsInHTML(html) {
 }
 
 
-function loadPong(){
-	const appDiv = document.querySelector("div#app");
-	if (!document.getElementById("pongCanvas")) {
-		console.log('does that happen ?');
-		// Create canvas if not already present
-		const canvas = document.createElement("canvas");
-		canvas.id = "pongCanvas";
-		canvas.width = 1000;
-		canvas.height = 500;
-		appDiv.innerHTML = ""; // Clear content before appending canvas
-		appDiv.appendChild(canvas);
-	}
-	// Initialize the game using the function from pong.js
-	if (typeof window.initializePong === "function") {
-		window.initializePong();
-	}
-}
 
-function loadShifumi(){
-	console.log("Shifumi loaded");
-	const appDiv = document.querySelector("div#app");
-	appDiv.innerHTML = "";
-
-	if (typeof window.initializeShifumi === "function") {
-		window.initializeShifumi();
-	}
-}
 
 async function fetchBody() {
-	switch (window.location.pathname){
-		case '/pong':
-			return loadPong();
-		case '/shifumi':
-			return loadShifumi();
-		default :
-			try {
-				const route = routes[window.location.pathname];
-				const response = await fetch(route);
-				if (!response.ok)
-					throw new Error(`${response.status}`);
-				const html = await response.text();
-				document.querySelector("div#app").innerHTML = html;
-				runScriptsInHTML(html);
-			} catch (error) { console.error(`${error}`); }
+	try {
+		const route = routes[window.location.pathname] || '/login'; // change login with 404
+		
+		const response = await fetch(route);
+
+		if (!response.ok)
+			throw new Error(`${response.status}`);
+
+		const html = await response.text();
+
+		document.querySelector("div#app").innerHTML = html;
+		runScriptsInHTML(html);
+	} catch (error) {
+		console.error(`${error}`);
 	}
 }
 
@@ -105,43 +75,6 @@ function AddAlert(message){
 	app.appendChild(alert);
 }
 
-async function fetchLogin(event) {
-	event.preventDefault();
-	var formData = new FormData(document.querySelector('form'));
-	const username = formData.get("username");
-	const password = formData.get("password");
-
-	// const cookies = 
-	// console.log(document.cookie);
-
-	try {
-		const response = await fetch('https://localhost:8443/api/login/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				// 'X-CSRFToken': csrfToken,
-			},
-			body: JSON.stringify({username: username,password: password}),
-		});
-		if (!response.ok){
-			response.json()
-			.then((data) => {
-				console.log(data);
-			})
-			AddAlert('Error on login. Try again.')
-			document.querySelector("Form").reset();
-		}
-		else {
-			window.history.pushState({}, "", '/home');
-			fetchBody();
-			updateNavbar();
-		}
-	}
-	catch (error) {
-		console.error(`${error}`); // Useless ?
-	}
-}
-
 
 async function checkIfAuthenticated() {
 	try {
@@ -159,43 +92,6 @@ async function checkIfAuthenticated() {
 		return false;
 	}
 }
-
-
-async function fetchLogout(event){
-	event.preventDefault();
-	try {
-		const response = await fetch('https://localhost:8443/api/logout/', {
-			method: 'GET',
-			credentials: 'same-origin',
-		});
-		if (!response.ok){
-			AddAlert('ALERT, this should never happen !');
-		}
-		window.history.pushState({}, "", '/login');
-		fetchBody();
-		updateNavbar();
-		console.log('logout success.');
-	}
-	catch (error) {
-		console.error(`${error}`); // Useless ?
-	}
-}
-
-document.addEventListener('click', function(event) {
-	if (event.target && event.target.classList.contains('login'))
-		fetchLogin(event);
-	if (event.target && event.target.classList.contains('logout'))
-		fetchLogout(event);
-	if (event.target && event.target.classList.contains('Register'))
-		fetchRegister(event);
-})
-
-
-// weird
-// window.onload = function() {
-// 	updateNavbar();
-// };
-
 
 async function updateNavbar() {
 	const isAuthenticated = await checkIfAuthenticated();
@@ -215,61 +111,4 @@ async function updateNavbar() {
 		else
 			link.style.display = 'none';
 	});
-}
-
-
-function AlertAndClean(message) {
-	AddAlert(message);
-}
-
-
-// Client-side validation of form (Server side should also be done)
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
-async function fetchRegister(event) {
-	event.preventDefault();
-	
-	const regex_email = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-	const regex_password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-
-	var formData = new FormData(document.querySelector('form'));
-	
-	const email = formData.get("email");
-	const email_confirmed = formData.get("email confirmation");
-	const username = formData.get("username");
-	const password = formData.get("password");
-	const password_confirmed = formData.get("password confirmation");
-	
-	if (!regex_email.exec(email))
-		return AddAlert('Error : Email syntax not respected.');
-	if (email != email_confirmed)
-		return AddAlert('Error : Emails different.');
-	if (!username)
-		return AddAlert('Error : Username empty.');
-	if (!regex_password.exec(password))
-		return AddAlert('Error : Password syntax not respected.');
-	if (password != password_confirmed)
-		return AddAlert('Error : Passwords different.');
-
-	try {
-		const response = await fetch('https://localhost:8443/api/register/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				// 'X-CSRFToken': csrfToken,
-			},
-			body: JSON.stringify({email:email, username: username,password: password}),
-		});
-		if (!response.ok){
-			AddAlert('Error on Register. Try again.')
-			document.querySelector("Form").reset();
-		}
-		else {
-			window.history.pushState({}, "", '/home');
-			fetchBody();
-			updateNavbar();
-		}
-	}
-	catch (error) {
-		console.error(`${error}`); // Useless ?
-	}
 }
--- static/js/shifumi.js
+++ static/js/shifumi.js
@@ -7,9 +7,10 @@ function initializeShifumi() {
   
 }
 
-if (window.location.pathname === "/shifumi") {
+if (window.location.pathname === "/shifumi") { // Not neccessary, you can just call initialize directly
 	initializeShifumi();
 }
 
+// I think useless
 // Optional: expose initializePong for router.js to call directly
 window.initializeShifumi = initializeShifumi;
