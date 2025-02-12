669563d45c0cd86a684bce99cc976a82b7c9f048
Leopoldo Vodak <lvodak@mi-r6-p3.s19.be>
2025-02-12 18:06:25 +0100

    Lobby test

--- static/js/Lobby.js
+++ static/js/Lobby.js
@@ -6,5 +6,5 @@ document.querySelector('#room-name-input').onkeyup = function(e) {
 };
 document.querySelector('#room-name-submit').onclick = function(e) {
     var roomName = document.querySelector('#room-name-input').value;
-    window.location.pathname = '/WaitingRoom/' + roomName + '/';
+    window.location.pathname = '/WaitingRoom/' + roomName;
 };
