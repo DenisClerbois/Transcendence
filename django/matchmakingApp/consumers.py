73da0eff57ee49498b3506b322590d69e7f90e0c
Leopoldo Vodak <lvodak@mi-r4-p5.s19.be>
2025-02-25 16:05:10 +0100

    GamePongLogic start implementing before branch

--- django/matchmakingApp/consumers.py
+++ django/matchmakingApp/consumers.py
@@ -1,20 +1,49 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 import asyncio
+from dataclasses import dataclass
+from math import sqrt
+
+@dataclass
+class Paddle:
+	width: int
+	height: int
+	speed: float
+
+@dataclass
+class Board:
+	x: int
+	y: int
+
+@dataclass
+class GameData:
+	board: Board
+	paddle: Paddle
+	ballRadius: float
+	initSpeed: int
 
 class GameLoopConsumer:
 	def __init__(self, game_id, channel_layer, p1, p2):
 		self.game_id = game_id
 		self.channel_layer = channel_layer
+		self.game_const = GameData(
+			board=Board(x=1000, y=500),
+			paddle=Paddle(width=12, height=100, speed=0),
+			ballRadius=10,
+			initSpeed=0,
+		)
 		self.game_state = {
-			'ball': [0, 0],
-			'vector': [0, 0],
+			'ball': [self.game_const.board.x / 2, self.game_const.board.y / 2],
+			'vector': [sqrt(0.5), sqrt(0.5)],
 			'score': [0, 0],
-			'paddle1': 0,
-			'paddle2': 0,
+			'speed': 5,
+			'paddle1': [self.game_const.board.y / 2 - self.game_const.paddle.height],
+			'paddle2': [self.game_const.board.y - self.game_const.paddle.width, self.game_const.board.y / 2 - self.game_const.paddle.height / 2],
 			p1: {'ArrowDown': False, 'ArrowUp': False},
 			p2: {'ArrowDown': False, 'ArrowUp': False},
 		}
+		self.game_const.initSpeed = self.game_state['speed']
+		self.game_const.paddle.speed = sqrt(self.game_state['vector'][0] ** 2 + self.game_state['vector'][1] ** 2) * self.game_const.initSpeed * 1.4
 		self.is_running = True
 	
 	def start_game_loop(self):
@@ -33,7 +62,32 @@ class GameLoopConsumer:
 	
 	def stop_game_loop(self):
 		self.is_running = False
+	def move_paddles(self, player1, player2):
+		if player1["keyhold"]:
+			if player1["action"] == "up":
+				self.check_paddle_movement(self.game_state["player1"]["paddle"][1], -1 * self.game_const.paddle.speed, "player1")
+			else:
+				self.check_paddle_movement(self.game_state["player1"]["paddle"][1], self.game_const.paddle.speed, "player1")
+		if player2["keyhold"]:
+			if player1["action"] == "up":
+				self.check_paddle_movement(self.game_state["player2"]["paddle"][1], -1 * self.game_const.paddle.speed, "player2")
+			else:
+				self.check_paddle_movement(self.game_state["player2"]["paddle"][1], self.game_const.paddle.speed, "player2")
 
+	def check_paddle_movement(self, pos, move, player):
+		if (pos + move) >= 0 and pos + move - self.game_const.paddle.height <= self.game_const.board.y:
+			self.game_state[player]["paddle"][1] += move
+	def move_ball(self):
+		if not self.check_collision():
+			self.game_state["ball"][0] += (self.game_state["vector"][0] * self.game_state["speed"])
+			self.game_state["ball"][1] += (self.game_state["vector"][1] * self.game_state["speed"])
+	def check_collision(self):
+		if self.game_state["ball"][1] + self.game_const.ballRadius + self.game_state["vector"][1] * self.game_state["speed"] > self.game_const.board.y:
+		elif self.game_state["ball"][1] - self.game_const.ballRadius - self.game_state["vector"][1] * self.game_state["speed"] < 0:
+	def increaseSpeed(self):
+		self
+	def scoreAndResetBall(self):
+		self
 
 active_game_loops = {}	
 
